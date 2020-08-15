187d852a23329dd4fd3e67e2039a99aa8c5d106a
David Lai <david@dlaisoft.com>
2020-08-14 23:23:43 -0700

    trying to get web working with ui-kitten

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  module: {
+    rules: [
+      // This would match almost any react-native module
+      {
+        test: /(@?react-(navigation|native)).*\.(ts|js)x?$/,
+        include: /node_modules/,
+        exclude: [/react-native-web/, /\.(native|ios|android)\.(ts|js)x?$/],
+        loader: 'babel-loader'
+      },
+      // This would match ui-kitten
+      {
+          test: /@?(ui-kitten|eva-design).*\.(ts|js)x?$/,
+          loader: 'babel-loader'
+      }
+    ]
+  }
 };
